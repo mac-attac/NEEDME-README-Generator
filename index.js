bca2324c08cd5ae5ef06debca41de339fbd77990
Steve McNamara <stephenmcnamara36@gmail.com>
2021-02-15 13:13:34 -0700

    updated licesning functions

--- index.js
+++ index.js
@@ -6,7 +6,52 @@ const path = require('path')
 
 // TODO: Create an array of questions for user input
 const questions = [
-    //the questions from the inquirer prompt go here
+    { 
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your repository?',
+    },
+    { 
+        type: 'input',
+        name: 'what',
+        message: 'What was your motivation in creating this repo?',
+    },
+    { 
+        type: 'input',
+        name: 'why',
+        message: 'Why did you build this project repo?',
+    },
+    { 
+        type: 'input',
+        name: 'how',
+        message: 'How does your project solve a problem and how does it do so?',
+    },
+    { 
+        type: 'input',
+        message: 'Are there any specific installation instructions for your repo?',
+        name: 'install',
+    },
+    { 
+        type: 'input',
+        message: 'Include any info on how your project should be used (enter screenshot links here, if desired).',
+        name: 'usage',
+    },
+    { 
+        type: 'input',
+        message: 'What is your name or the name of your organization you would like to create a license under?',
+        name: 'licenseName',
+    },
+    { 
+        type: 'input',
+        message: 'Include any info on how your project should be used (enter screenshot links here, if desired).',
+        name: 'usa',
+    },
+    { 
+        type: 'checkbox',
+        message: 'Which type of license would you like for your repo?',
+        choices: ['Apache License 2.0','GNU General Public License v3.0', 'MIT License', 'None'],
+        value: 'licenseValue'
+    },
 ];
 
 // TODO: Create a function to write README file
@@ -14,10 +59,16 @@ function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 }
 
+//Create an addLicense function to generate a .txt file with the License info inside
+function addLicense() {
+
+}
+
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
         .then(answers => {writeToFile('README.md', generateMarkdown(answers))})
+        .then(fs.writeFile('LICENSE.txt', addLicense(answers)))
 
 }
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,31 +1,97 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license !== 'None') {
-    return `License Badge: ${license.badge}`
+  if (license === 'Apache License 2.0') {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  else if (license === 'GNU General Public License v3.0') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
+  else if (license === 'MIT License') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  }
+  else {
+    return ``
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'Apache License 2.0') {
+    return `Licensed under the [Apache License 2.0](LICENSE.txt) license.`
+  }
+  else if (license === 'GNU General Public License v3.0') {
+    return `Licensed under the [GNU General Public License v3.0](LICENSE.txt) license.`
+  }
+  else if (license === 'MIT License') {
+    return `Licensed under the [MIT License](LICENSE.txt) license.`
+  }
+  else {
+    return ``
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'Apache License 2.0') {
+    return `Copyright (c) ${data.licenseName}. All rights reserved.`
+  }
+  else if (license === 'GNU General Public License v3.0') {
+    return `Copyright (c) ${data.licenseName}. All rights reserved.`
+  }
+  else if (license === 'MIT License') {
+    return `Copyright (c) ${data.licenseName}. All rights reserved.`
+  }
+  else {
+    return `This repository is not under any licensing.`
+  }
+}
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  title of my project and sections entitled 
-  ##Description
-  ##Table of Contents
-  ##Installation
-  ##Usage
-  ##License
-  ##Contributing 
-  ##Tests
-  ##Questions
+  return `
+${renderLicenseBadge(data.licenseValue)}
+#${data.title}
+
+##Description
+
+${data.what}
+
+${data.why}
+
+${data.how}
+
+##Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+##Installation
+
+${data.install}
+
+##Usage
+
+${data.usage}
+
+##License
+
+${renderLicenseSection()}
+
+${renderLicenseLink()}
+
+##Contributing 
+
+${data.contributing}
+  
+##Tests
+${data.tests}
 
 `;
 }
