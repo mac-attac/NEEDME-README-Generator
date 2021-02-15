bfe47ae7d9573a823d1505c04ef9d4b6bbef74a8
Steve McNamara <stephenmcnamara36@gmail.com>
2021-02-15 16:10:51 -0700

    generated README for presentation

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# NEEDME-README-Generator
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)
+
+#NEEDME.md - A README Generator
+
+##Description
+
+To generate good READMEs
+
+READMEs take a lot of time to generate, this helps to cut down on the time.
+
+It saves time by creating READMEs
+
+##Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+##Installation
+
+We need to use "npm i inquirer" to initialize the npm module
+
+##Usage
+
+Should be used to help facilitate README generation.
+
+##Credits
+
+Me, myself, and I.
+
+##License
+
+Copyright (c) Stephen McNamara. All rights reserved.
+
+Licensed under the [MIT License](LICENSE.txt) license.
+
+##Contributing 
+
+Feel free to contribute to this repository by logging bugs, submitting a pull request, or leaving a comment.
+
+This project has adopted the [Contributor Covenant](https://www.contributor-covenant.org/) guidelines for contribution. Please see their [FAQ](https://www.contributor-covenant.org/faq/) for any additional questions.
+  
+##Tests
+
+ I did not create any tests to run on this project.
+
+##Questions
+
+If you have any questions about this project, contact me on [GitHub](github.com/mac-attac) or by email at stephenmcnamara@fakemail.com.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license === 'Apache License 2.0') {
+function renderLicenseBadge(data) {
+  if (data.licenseValue === 'Apache License 2.0') {
     return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
   }
-  else if (license === 'GNU General Public License v3.0') {
+  else if (data.licenseValue === 'GNU General Public License v3.0') {
     return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
   }
-  else if (license === 'MIT License') {
+  else if (data.licenseValue === 'MIT License') {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
   }
   else {
@@ -16,14 +16,14 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === 'Apache License 2.0') {
+function renderLicenseLink(data) {
+  if (data.licenseValue === 'Apache License 2.0') {
     return `Licensed under the [Apache License 2.0](LICENSE.txt) license.`
   }
-  else if (license === 'GNU General Public License v3.0') {
+  else if (data.licenseValue === 'GNU General Public License v3.0') {
     return `Licensed under the [GNU General Public License v3.0](LICENSE.txt) license.`
   }
-  else if (license === 'MIT License') {
+  else if (data.licenseValue === 'MIT License') {
     return `Licensed under the [MIT License](LICENSE.txt) license.`
   }
   else {
@@ -32,14 +32,14 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license === 'Apache License 2.0') {
+function renderLicenseSection(data) {
+  if (data.licenseValue === 'Apache License 2.0') {
     return `Copyright (c) ${data.licenseName}. All rights reserved.`
   }
-  else if (license === 'GNU General Public License v3.0') {
+  else if (data.licenseValue === 'GNU General Public License v3.0') {
     return `Copyright (c) ${data.licenseName}. All rights reserved.`
   }
-  else if (license === 'MIT License') {
+  else if (data.licenseValue === 'MIT License') {
     return `Copyright (c) ${data.licenseName}. All rights reserved.`
   }
   else {
@@ -48,25 +48,26 @@ function renderLicenseSection(license) {
 }
 
 //create function for option to adopt Contributor Covenant if desired
-function addContributorCovenant(confirm) {
-  if (confirm === true) {
+function addContributorCovenant(data) {
+  if (data.confirmContributorCovenant === true) {
     return `This project has adopted the [Contributor Covenant](https://www.contributor-covenant.org/) guidelines for contribution. Please see their [FAQ](https://www.contributor-covenant.org/faq/) for any additional questions.`
   } else {
   }
 }
 
-function addCCBadge(confirm) {
-  if (confirm === true) {
+function addCCBadge(data) {
+  if (data.confirmContributorCovenant === true) {
     return `[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)`
   } else {
   }
 }
 
 
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-${renderLicenseBadge(data.licenseValue)}${addCCBadge(data.confirmContributorCovenant)}
+${renderLicenseBadge(data)}${addCCBadge(data)}
 
 #${data.title}
 
@@ -99,19 +100,23 @@ ${data.credit}
 
 ##License
 
-${renderLicenseSection()}
+${renderLicenseSection(data)}
 
-${renderLicenseLink()}
+${renderLicenseLink(data)}
 
 ##Contributing 
 
 Feel free to contribute to this repository by logging bugs, submitting a pull request, or leaving a comment.
 
-${addContributorCovenant()}
+${addContributorCovenant(data)}
   
 ##Tests
+
 ${data.tests}
 
+##Questions
+
+If you have any questions about this project, contact me on [GitHub](github.com/${data.username}) or by email at ${data.email}.
 `;
 }
 
